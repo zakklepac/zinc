f5490f5d663321a5aece4ac244e6ec540abbfdf9
Marc Majcher <majcher@gmail.com>
2018-07-30 01:15:01 -0500

    soln

--- 01-Components/zinc.js
+++ 01-Components/zinc.js
@@ -3,8 +3,27 @@
 /* eslint-env browser */
 
 (() => {
+    const userTemplate = `
+    <li class="user">
+        <img class="user-photo" src="{{picture.thumbnail}}" alt="Photo of {{name.first}} {{name.last}}">
+        <div class="user-name">{{name.first}} {{name.last}}</div>
+        <div class="user-location">{{location.city}}, {{location.state}}</div>
+        <div class="user-email">{{email}}</div>
+    </li>
+    `;
+
+    function renderTemplate(template, data) {
+        return template.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
+            variable.split('.').reduce((acc, curr) => acc[curr], data) || ''
+        );
+    }
+
     function populateList(results) {
-        console.log(results); // eslint-disable-line no-console
+        const userList = document.getElementById('z-user-list');
+        results.forEach((user) => {
+            const html = renderTemplate(userTemplate, user);
+            userList.insertAdjacentHTML('beforeend', html);
+        });
     }
 
     function init() {
