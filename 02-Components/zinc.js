efb182e42ed0b099d583d1ec67de29a8d7a118a8
Marc Majcher <majcher@gmail.com>
2018-07-30 02:14:25 -0500

    soln

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -18,12 +18,21 @@
         email: 'jack.burton@example.com'
     };
 
-    function renderComponent(element, content) {
-        console.log(element, content); // eslint-disable-line no-console
+    function renderTemplate(template, data) {
+        return fetch(`${template}.html`)
+            .then(res => res.text())
+            .then(html => html.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
+            variable.split('.').reduce((acc, curr) => acc[curr], data) || ''));
+    }
+
+    function renderComponent(element, templateFile, content) {
+        const el = document.querySelector(element);
+        renderTemplate(templateFile, content)
+            .then(html => el.insertAdjacentHTML('beforeend', html));
     }
 
     function init() {
-        renderComponent('user-item', userData);
+        renderComponent('user-item', 'user', userData);
     }
 
     document.addEventListener('DOMContentLoaded', init);
