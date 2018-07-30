7f837444b42a517a26837cb285f4a95f28c66273
Marc Majcher <majcher@gmail.com>
2018-07-30 01:38:50 -0500

    soln

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -3,26 +3,18 @@
 /* eslint-env browser */
 
 (() => {
-    const userTemplate = `
-    <li class="user">
-        <img class="user-photo" src="{{picture.thumbnail}}" alt="Photo of {{name.first}} {{name.last}}">
-        <div class="user-name">{{name.first}} {{name.last}}</div>
-        <div class="user-location">{{location.city}}, {{location.state}}</div>
-        <div class="user-email">{{email}}</div>
-    </li>
-    `;
-
     function renderTemplate(template, data) {
-        return template.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
-            variable.split('.').reduce((acc, curr) => acc[curr], data) || ''
-        );
+        return fetch(`${template}.html`)
+            .then(res => res.text())
+            .then(html => html.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
+            variable.split('.').reduce((acc, curr) => acc[curr], data) || ''));
     }
 
     function populateList(results) {
         const userList = document.getElementById('z-user-list');
         results.forEach((user) => {
-            const html = renderTemplate(userTemplate, user);
-            userList.insertAdjacentHTML('beforeend', html);
+            renderTemplate('user', user).then(html =>
+            userList.insertAdjacentHTML('beforeend', html));
         });
     }
 
