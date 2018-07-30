2887f7bf3d76cdf69c170620db4cb89b9b5c6db6
Marc Majcher <majcher@gmail.com>
2018-07-30 00:57:28 -0500

    soln

--- 01-Components/zinc.js
+++ 01-Components/zinc.js
@@ -3,9 +3,38 @@
 /* eslint-env browser */
 
 (() => {
-    function populateList(results) {
-        console.log(results); // eslint-disable-line no-console
+    const userTemplate = `
+    <li class="user">
+        <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+        <div class="user-name">{{ firstName }} {{ lastName }}</div>
+        <div class="user-location">{{ city }}, {{ state }}</div>
+        <div class="user-email">{{ email }}</div>
+    </li>
+    `;
+
+    function renderTemplate(template, data) {
+        return template.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, variable) => {
+            return data[variable] || '';
+        });
     }
+
+    function populateList(results) {
+        const userList = document.getElementById('z-user-list');
+        results.forEach((user) => {
+            const data = {
+                photo: user.picture.thumbnail,
+                firstName: user.name.first,
+                lastName: user.name.last,
+                city: user.location.city,
+                state: user.location.state,
+                email: user.location.email
+            };
+
+            const html = renderTemplate(userTemplate, data);
+            userList.insertAdjacentHTML('beforeend', html);
+        });
+    }
+
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
