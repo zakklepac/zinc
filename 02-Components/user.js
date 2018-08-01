a3743716e5dd37c113a6e875c4d6b6e21ac2b7f5
Marc Majcher <majcher@gmail.com>
2018-07-31 22:27:35 -0500

    user two

--- 02-Components/user.js
+++ 02-Components/user.js
@@ -18,7 +18,23 @@
         },
         email: 'jack.burton@example.com'
     };
+    
+    const userData2 = {
+        picture: {
+            thumbnail: 'https://media.newyorker.com/photos/597611914867016af4a67cb2/master/w_727,c_limit/Aguirre-Octavia-Butler_01.jpg'
+        },
+        name: {
+            first: 'Octavia',
+            last: 'Butler'
+        },
+        location: {
+            city: 'Lake Forest Park',
+            state: 'WA'
+        },
+        email: 'octavia.butler@example.com'
+    };
 
     Zinc.registerComponent('user-item', 'user', userData);
+    Zinc.registerComponent('user-two', 'user', userData2);
 })();
 
