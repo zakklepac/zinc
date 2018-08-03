7ae9a72fe11ef58574194f2dea263185f8f77d61
Marc Majcher <majcher@gmail.com>
2018-08-03 11:24:55 -0500

    soln

--- 02-Components/user.js
+++ 02-Components/user.js
@@ -4,18 +4,27 @@
 /* globals Zinc */
 
 (() => {
-    const userData = {
-        picture: {
-            thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
-        },
-        name: {
-            first: 'Jack',
-            last: 'Burton'
-        },
-        location: {
-            city: 'San Francisco',
-            state: 'CA'
-        },
-        email: 'jack.burton@example.com'
-    };
+    function userController() {
+        this.addEventListener('click', () => {
+            this.classList.toggle('hilight');
+            this.$state.hilit = !this.$state.hilit;
+        });
+    }
+
+    function populateList(users) {
+        const myComponents = ['user-one', 'user-two', 'user-three', 'user-four', 'user-five'];
+        for (let i = 0; i < users.length; i++) {
+            Zinc.registerComponent({
+                name: myComponents[i],
+                templateFile: 'user',
+                data: users[i],
+                controller: userController
+            });
+        }
+        Zinc.renderComponents();
+    }
+
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(json => populateList(json.results));
 })();
--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,17 +1,63 @@
 'use strict';
 
 /* eslint-env browser */
+/* eslint-disable no-unused-vars */
 
-const Zinc = {};
+const Zinc = {
+    components: {}
+};
 
 (() => {
-    function renderComponent(element, content, userData) {
-        console.log(element, content); // eslint-disable-line no-console
-    }
+    const domParser = new DOMParser();
 
     function init() {
-        renderComponent('user-item', 'user', userData);
+        Zinc.renderComponents();
     }
+
+    function renderTemplate(template, data) {
+        return fetch(`${template}.html`)
+            .then(res => res.text())
+            .then(html => html.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (match, variable) =>
+                variable.split('.').reduce((acc, curr) => acc[curr], data) || ''));
+    }
+
+    Zinc.renderComponent = (componentName) => {
+        const component = Zinc.components[componentName];
+        const nodeList = document.querySelectorAll(componentName);
+        for (let i = 0; i < nodeList.length; i++) {
+            renderTemplate(component.templateFile, component.data)
+                .then((html) => {
+                    const doc = domParser.parseFromString(html, 'text/html');
+                    const el = nodeList[i].insertAdjacentElement('beforeend', doc.firstChild.children[1].firstChild);
+                    el.$state = {};
+                    if (component.controller) {
+                        el.$controller = component.controller;
+                        el.$controller();
+                    }
+                    Zinc.components[componentName].element = el;
+                });
+        }
+    };
+
+    Zinc.renderComponents = () => {
+        Object.values(Zinc.components).forEach((component) => {
+            Zinc.renderComponent(component.name);
+        });
+    };
+
+    Zinc.registerComponent = ({
+        name,
+        templateFile,
+        data,
+        controller
+    }) => {
+        Zinc.components[name] = {
+            name,
+            templateFile,
+            data,
+            controller
+        };
+    };
 
     document.addEventListener('DOMContentLoaded', init);
 })();
