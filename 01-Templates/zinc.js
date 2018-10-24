f61b76dfb4f5fb7d585897ae7a0fece828f43a32
zakklepac <41202649+zakklepac@users.noreply.github.com>
2018-10-24 16:04:46 -0500

    you already know

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -3,12 +3,30 @@
 /* eslint-env browser */
 
 (() => {
+    const userTemplate = `
+<li class="user">
+    <img class="user-photo" src="{{ picture.thumbnail }}" alt="Photo of {{ name.first }} {{ name.last }}">
+    <div class="user-name">{{ name.first }} {{ name.last }}</div>
+    <div class="user-location">{{ location.city }}, {{ location.state }}</div>
+    <div class="user-email">{{ email }}</div>
+</li>
+    `;
+
+    function renderTemplate(template, data) {
+        return template.replace(/\{\{\s*([\w.]+)\s*\}\}/g, (thing, variable) => 
+        variable.split('.').reduce((acc, currVal) => acc[currVal], data));
+    }
+
     function populateList(results) {
-        console.log(results); // eslint-disable-line no-console
+        const userList = document.getElementById('z-user-list');
+        results.forEach(user => {
+            let renderedTemplate = renderTemplate(userTemplate, user);
+            userList.insertAdjacentHTML("beforeend", renderedTemplate);
+        });
     }
 
     function init() {
-        fetch('https://randomuser.me/api/?results=5')
+        fetch('https://randomuser.me/api/?results=3')
             .then(res => res.json())
             .then(json => populateList(json.results));
     }
